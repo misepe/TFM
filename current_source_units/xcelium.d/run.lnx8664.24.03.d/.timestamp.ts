47ed435113232b4db96d92a4c1a613f6d9252ca1
msegper <msegper@teleco.upv.es>
2026-01-22 21:01:04 +0100

    model all non linearities done

--- current_source_units/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
+++ current_source_units/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
@@ -1,2 +1,2 @@
+1769111283 /home/msegper/Documents/TFM/current_source_units/models/currentSourceUnits_all_non_linearities.sv
 1762853391 /home/msegper/Documents/TFM/current_source_units/tb/currentSourceUnits_tb.sv
-1769109336 /home/msegper/Documents/TFM/current_source_units/models/currentSourceUnits_noise.sv
--- current_source_units/xrun.f
+++ current_source_units/xrun.f
@@ -2,7 +2,8 @@
 //./eda/cadence/2024-25/RHELx86/XCELIUM_24.03.004/tools.lnx86/affirma_ams/etc/dms/cds_rnm_pkg.sv
 //./models/currentSourceUnits.sv           // original digital vco
 //./models/currentSourceUnits_mismatch.sv           // original digital vco
-./models/currentSourceUnits_noise.sv           // original digital vco
+//./models/currentSourceUnits_noise.sv           // original digital vco
+./models/currentSourceUnits_all_non_linearities.sv           // original digital vco
 ./tb/currentSourceUnits_tb.sv             // Testbench
 -access +rw                 // Turn on read/write object access 
 //-gui                        // Performs simulation in graphical mode
